da283525c9bd1686a02d3c9745d73ee3d035e9a4
Abdulqadir <abdulqadirlalparia@gmail.com>
2024-01-27 18:10:52 +0530

    changes

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ function multiply (a , b){
 function divide (a , b){
     return a / b;
 }
-let a = -9;
-let b = 3;
+let a = 2;
+let b = 2;
 
-let num1,operator,num2;
+let num1,num2;
 
 function operate(a,o,b){
 
@@ -33,4 +33,24 @@ function operate(a,o,b){
         return alert("error");
     }
 }
-console.log(operate(2,'/',9));
+
+
+
+let opvalue;
+const display = document.getElementById('display');
+const btns = document.querySelectorAll("div.operators .num");
+btns.forEach((item)=>{
+    item.addEventListener('click', ()=> {
+        const value = item.textContent;
+        display.textContent += value;
+        disvalue = display.textContent;
+    })
+})
+const operators = document.querySelectorAll('div.operators .op');
+console.log(operators);
+operators.forEach((op)=>{
+    op.addEventListener('click',()=>{
+        opvalue = op.textContent;
+        return opvalue;
+    })
+})
